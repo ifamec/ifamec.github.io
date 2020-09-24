22d6404ba4a4c90bb2372aa38bb18b9e430011c6
zzh <awjaax@gmail.com>
2020-09-23 21:01:50 -0700

    fix - bail ie

--- tool/index.js
+++ tool/index.js
@@ -1,15 +1,3 @@
-(() => {
-	const isIE = window.navigator.userAgent.indexOf("MSIE ");
-	console.log(window.navigator.userAgent)
-	if (isIE > 0 || !!navigator.userAgent.match(/Trident.*rv:11\./)) {
-		alert('NO IE')
-		location.href = location.hostname
-	}
-})()
-// BAIL IF IE
-
-
-
 import * as csv2md from './src/csv2md.js'
 
 csv2md.csv2md_ui('#content')
